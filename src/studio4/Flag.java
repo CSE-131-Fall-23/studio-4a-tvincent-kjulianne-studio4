8c2d3aa00f6e28f007c2a3ae553596566da53f28
julianne <julianne@10.232.176.9>
2023-10-05 10:50:59 -0500

    flag
    we made a flag. it was a red rectangle with a blue square in the top
    left hand corner with a yellow circle within the blue square.

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,15 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(1.0, 1.0, 1.0, 1.0); 
 		
+		StdDraw.setPenColor(Color.BLUE); 
+		StdDraw.filledRectangle(0.0, 1.0, 0.4, 0.4); 
+		
+		Color yellow = new Color (255, 255, 0); 
+		StdDraw.setPenColor(yellow); 
+		StdDraw.setPenRadius(0.2); 
+		StdDraw.point(0.2,0.8);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -9,9 +10,6 @@ import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
 
-/**
- * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
- */
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -19,6 +17,24 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next(); 
+		int redComponent = in.nextInt(); 
+		int greenComponent = in.nextInt(); 
+		int blueComponent = in.nextInt(); 
+		boolean isFilled = in.nextBoolean(); 
+		double parameterOne = in.nextDouble(); 
+		double parameterTwo = in.nextDouble(); 
+		double parameterThree = in.nextDouble(); 
+		double parameterFour = in.nextDouble(); 
+		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent); 
+		if(isFilled) { 
+			StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		} else { 
+			StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		}
+			
+
 		
 	}
 }
